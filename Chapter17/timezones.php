9b3b1faa108ab96766a64d85d10632b6cf17e220
Tantantantan <j.magay@klab.com>
2014-07-03 11:27:24 +0800

    Update timezones.php
    use date_default_timezone_get() instead of ini_get('date.timezone')

--- Chapter17/timezones.php
+++ Chapter17/timezones.php
@@ -1,12 +1,7 @@
 <?php
-$tz = ini_get('date.timezone');
+$tz = date_default_timezone_get();
 $dtz = new DateTimeZone($tz);
-$past = new DateTime("2009-02-12 16:42:33", $dtz);
-$current = new DateTime("now", $dtz);
-// creates a new instance of DateInterval
-$diff = $past->diff($current);
-$pastString = $past->format("Y-m-d");
-$currentString = $current->format("Y-m-d");
-$diffString = $diff->format("%yy %mm, %dd");
-echo "Difference between {$pastString} and {$currentString} is {$diffString}";
+$dt = new DateTime("now", $dtz);
+
+echo "exam today: ".$dt->format("Y-m-d h:i:s");
 ?>
